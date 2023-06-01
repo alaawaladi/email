055927b62f3821af422e590a2de1f27cdf09efb0
Alae Edine <32775934+alaawaladi@users.noreply.github.com>
2023-06-01 15:53:22 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from models import EmailOptions, EmailPayload
+app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000","http://0.0.0.0:8000"],  # Adjust this based on your requirements
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class EmailPayload(BaseModel):
+    subject: str
+    email_list: str
+    body: str
+
+class EmailOptions(BaseModel):
+    selected_profiles: str
+    browser_language: str
+    send_limit_per_profile: int
+    loop_profile: int
+
+@app.post("/send_email")
+async def send_email(payload: EmailPayload, options: EmailOptions):
+    # Print the received payload and options
+    print("*"*50,"api.py","*"*50)
+    
+    print("Received Payload:", payload,"\n")
+    
+    print("Received Options:", options,"\n")
+    
+    print("*"*50,"api.py","*"*50)
+
+    # Process the data and send the response
+
+    # Return a response indicating success or failure
+    return {"message": "Email sent successfully"}
+
